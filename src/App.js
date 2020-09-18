9eb6234b25d930c217016e46188479001065bf7b
srikanth4c <srikanthbinf@gmail.com>
2020-09-18 13:57:55 +0530

    UPDATE: input ele dynamically added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
+import InputBox from './components/inputBox'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Credit Card Number</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      cardNo: undefined
+    }
+  }
+  getCreditCardNum=(value)=>{
+    this.setState({
+      cardNo:value
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>Credit Card Number</h1>
+        <div className='boxContainer'>
+          <InputBox inputBoxCount={4} getCreditCardNum={this.getCreditCardNum}/>
+        </div>
+        <h3>{this.state.cardNo}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
